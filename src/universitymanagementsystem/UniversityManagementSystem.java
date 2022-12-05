2ac5831ee4e89548bdab2c87dc11cad3b32f42eb
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2022-12-05 19:54:19 +0200

    first commit

---
+++ src/universitymanagementsystem/UniversityManagementSystem.java
@@ -0,0 +1,216 @@
+package universitymanagementsystem;
+
+import java.util.Scanner;
+
+public class UniversityManagementSystem {
+
+    static Scanner input;
+    static int selected;
+
+    // Admin authentication
+    static String adminName = "Mohammed";
+    static String adminPassword = "123456";
+
+    // Department information
+    int[] departmentIds = new int[100];
+    String[] departmentNames = new String[100];
+
+    // Course information
+    int[] courseIds = new int[100];
+    String[] courseNames = new String[100];
+    String[] courseCode = new String[100];
+    double[] courseNumberHours = new double[100];
+
+    // Start app in this fnction
+    public static void main(String[] args) {
+        while (true) {
+            if (mainFunction() == 4) {
+                break;
+            }
+        }
+    }
+
+    // mainFunction will container all main app elements
+    public static int mainFunction() {
+        input = new Scanner(System.in);
+
+        welcoming("User");
+        do {
+            selected = selection(textFirstSelection());
+            switch (selected) {
+                case 1: {
+                    adminAuth();
+                }
+                case 2: {
+                    instructor();
+                }
+                case 3: {
+                    student();
+                }
+            }
+            System.out.println("\n\n---------------------------------------------\n\n");
+        } while (selected != 4);
+        return selected;
+    }
+
+    // This function taken name and return message welcoming with email
+    public static void welcoming(String name) {
+        System.out.println("\n=========== Welcome " + name + " ===========\n");
+    }
+
+    // This function taken name and return message welcoming with email
+    public static int selection(String text) {
+        System.out.println(text);
+        int s = input.nextInt();
+        System.out.println("\n\n");
+        return s;
+    }
+
+    // Returned first options to selection
+    public static String textFirstSelection() {
+        return "1. Admin.\n"
+                + "2. Instructor.\n"
+                + "3. Student.\n"
+                + "4. Exit.\n"
+                + "Please select your choice[1, 2 or 3]:";
+    }
+
+    // Check authientication
+    public static void adminAuth() {
+        welcoming("Admin");
+        System.out.print("Enter username: ");
+        String name = input.next();
+        System.out.print("\nEnter password: ");
+        String password = input.next();
+        if (name.equalsIgnoreCase(adminName)
+                && password.equalsIgnoreCase(adminPassword)) {
+            admin();
+        } else {
+            System.out.println("\n  ^_^ Sorry, the data is incorrect ^_^  ");
+            System.out.println("  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  ");
+        }
+    }
+
+    // Show admin options
+    public static void admin() {
+        welcoming(adminName);
+        int select = 0;
+        do {
+            select = selection(textSelectionAdmin());
+            switch (select) {
+                case 1: {
+                    add();
+                }
+                case 2: {
+                    assgin();
+                }
+                case 3: {
+                    show();
+                }
+                case 4: {
+                    update();
+                }
+                case 5: {
+                    delete();
+                }
+                case 6: {
+                    search();
+                }
+            }
+        } while (select != 7);
+
+    }
+
+    // Returned for show admin option
+    public static String textSelectionAdmin() {
+        return "\n"
+                + "1. Add.\n"
+                + "2. Assgin.\n"
+                + "3. Show.\n"
+                + "4. Update.\n"
+                + "5. Delete.\n"
+                + "6. Search.\n"
+                + "7. Exit.\n"
+                + "Please select your choice[1, 2, 3, 4, 5, 6 or 7]:";
+    }
+
+    // Show add options
+    public static void add() {
+        System.out.println("~~~~~~~~~~~~~~ Choose the thing you want to add to it ~~~~~~~~~~~~~~");
+        int select = 0;
+        do {
+            select = selection(textSelectionAdminAdd());
+            switch (select) {
+                case 1: {
+                    addDepartments();
+                }
+                case 2: {
+                    addCourse();
+                }
+                case 3: {
+                    addInstructor();
+                }
+                case 4: {
+                    addStudent();
+                }
+            }
+        } while (select != 5);
+
+    }
+
+    // Returned text for add 
+    public static String textSelectionAdminAdd() {
+        return "\n"
+                + "1. Add Departments.\n"
+                + "2. Add Course.\n"
+                + "3. Add Instructor.\n"
+                + "4. Add Student.\n"
+                + "5. Exit.\n"
+                + "Please select your choice[1, 2, 3, 4 or 5]:";
+    }
+
+    public static void addDepartments() {
+
+    }
+
+    public static void addCourse() {
+
+    }
+
+    public static void addInstructor() {
+
+    }
+
+    public static void addStudent() {
+
+    }
+
+    public static void assgin() {
+
+    }
+
+    public static void show() {
+
+    }
+
+    public static void update() {
+
+    }
+
+    public static void delete() {
+
+    }
+
+    public static void search() {
+
+    }
+
+    public static void instructor() {
+
+    }
+
+    public static void student() {
+
+    }
+
+}
